00a6401838ec3d4bba86749f889eb4e3816e4bae
nichia <nichia@gmail.com>
2018-11-14 11:33:26 -0500

    Add strong params for additional user fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # redirect users to the origin or root_path if the origin path is nil
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || root_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name])
   end
 end
