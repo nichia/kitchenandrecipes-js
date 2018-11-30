128f688db4cf038937ef7d13e4fb3403c2faaaad
nichia <nichia@gmail.com>
2018-11-30 10:50:16 -0500

    Update spec.md

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root "welcome#home"
+
   devise_for :users, :controllers => {
     omniauth_callbacks: "users/omniauth_callbacks"
   }
-
-  root to: "welcome#home"
 
   resources :recipes
 
