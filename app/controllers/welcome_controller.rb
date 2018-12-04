08aeb118b6dd0edaaef2b184207a566e8df2bc23
nichia <nichia@gmail.com>
2018-12-04 18:21:34 -0500

    Tidy recipe index display page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,9 +2,10 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    if current_user.blank?
-      redirect_to new_user_session_url
-    end
+    # if current_user.blank?
+    #   redirect_to new_user_session_url
+    # end
+    @recipes = Recipe.all_public
   end
 
 end
