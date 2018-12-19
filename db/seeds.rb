7ee405dc98a35f8dbbfe74ae21dfc5bf7350f68d
nichia <nichia@gmail.com>
2018-12-19 00:06:17 -0500

    Fix display of recipe time and fix seed to add avatar/image at create instead of attach for to pass validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,18 +20,26 @@ Category.create(category_type: 'meal', name: 'side')
 
 # Create users
 avatars = Dir.glob('storage/avatars/*.jpg')
+icon = avatars.sample
 
-user = User.create(
+usr = User.create(
   name: "lorem",
   email: "lorem@email.com",
   password: "Password1!",
   first_name: "Lorem",
   last_name: "Ipsum"
-)
-icon = avatars.sample
-user.avatar.attach(io: File.open(icon), filename: icon.split(/\//).last, content_type: ['image/gif', 'image/png', 'image/jpg', 'image/jpeg'])
+) do |user|
+  Rails.root.join(icon).open('rb') do |io|
+    user.avatar.attach(io: io, filename: icon.split(/\//).last, content_type: ['image/jpg'])
+ end
+end
+# do |user| user.avatar.attach(io: "File.open(icon)", filename: "icon.split(/\//).last", content_type: ['image/jpg']) end
 
-user = User.create(
+# icon = avatars.sample
+# usr.avatar.attach(io: File.open(icon), filename: icon.split(/\//).last, content_type: ['image/gif', 'image/png', 'image/jpg', 'image/jpeg'])
+
+icon = avatars.sample
+usr = User.create(
   name: "CDion",
   email: Faker::Internet.safe_email("celine.dion"),
   password: "#{Faker::Internet.password(8)}A1@",
@@ -39,20 +47,25 @@ user = User.create(
   first_name: "Celine",
   last_name: "Dion",
   uid: Faker::Number.between(100000, 1000000)
-)
-icon = avatars.sample
-user.avatar.attach(io: File.open(icon), filename: icon.split(/\//).last, content_type: ['image/gif', 'image/png', 'image/jpg', 'image/jpeg'])
+) do |user|
+  Rails.root.join(icon).open('rb') do |io|
+    user.avatar.attach(io: io, filename: icon.split(/\//).last, content_type: ['image/jpg'])
+ end
+end
 
 10.times do
-  user = User.create(
+  icon = avatars.sample
+  usr = User.create(
     name: Faker::Internet.username(8, %w(. _)),
     email: Faker::Internet.unique.safe_email,
     password: "#{Faker::Internet.password(8)}A1@",
     first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-  )
-  icon = avatars.sample
-  user.avatar.attach(io: File.open(icon), filename: icon.split(/\//).last, content_type: ['image/gif', 'image/png', 'image/jpg', 'image/jpeg'])
+    last_name: Faker::Name.last_name
+  ) do |user|
+    Rails.root.join(icon).open('rb') do |io|
+      user.avatar.attach(io: io, filename: icon.split(/\//).last, content_type: ['image/jpg'])
+   end
+  end
 end
 
 # Measurement
@@ -88,17 +101,21 @@ yields = ['makes', 'serves']
 preps = ['cut', 'slice', 'shred', 'squeeze', 'chopped', 'grate', 'minced']
 images = Dir.glob('storage/images/*.jpg')
 100.times do
+  icon = images.sample
   recipe = Recipe.create(
     name: "#{Faker::Food.dish.downcase} #{Faker::Lorem.unique.word}",
     description: Faker::Food.description,
-    prep_time: Faker::Coffee.intensifier,
-    cook_time: Faker::Coffee.intensifier,
+    prep_time: Faker::Time.between(DateTime.now - 2, DateTime.now),
+    cook_time: Faker::Time.between(DateTime.now - 2, DateTime.now),
     yield: yields.sample,
     yield_size: Faker::Coffee.blend_name,
     user: User.find_by_id(Faker::Number.between(1, 12))
-  )
-  icon = images.sample
-  recipe.image.attach(io: File.open(icon), filename: icon.split(/\//).last, content_type: ['image/gif', 'image/png', 'image/jpg', 'image/jpeg'])
+  ) do |r|
+    Rails.root.join(icon).open('rb') do |io|
+      r.image.attach(io: io, filename: icon.split(/\//).last, content_type: ['image/gif', 'image/png', 'image/jpg', 'image/jpeg'])
+   end
+  end
+  # recipe.image.attach(io: File.open(icon), filename: icon.split(/\//).last, content_type: ['image/gif', 'image/png', 'image/jpg', 'image/jpeg'])
 
   recipe.recipe_categories.create(
     category: Category.find_by_id(Faker::Number.between(1, 6))
