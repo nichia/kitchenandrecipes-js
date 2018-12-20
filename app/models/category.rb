be288be8d6756d72a933886bfa31cff824d2e0eb
nichia <nichia@gmail.com>
2018-12-19 23:45:34 -0500

    Tidy navbar, comments and add image wordings

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,9 +3,9 @@ class Category < ApplicationRecord
   has_many :recipes, through: :recipe_categories
 
   scope :by_type, -> { select(:id, :category_type, :name).order(category_type: :asc, name: :asc) }
-
   scope :group_by_type, -> { by_type.group_by { |c| c.category_type }}
-
+  scope :by_meal_type, -> { where(category_type: "meal").order(name: :asc) }
+  
   def name_with_type
     "#{category_type}: #{name}"
   end
