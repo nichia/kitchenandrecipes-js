69965dde8c8d981ebc7bcb1cd0fbe7545354c216
nichia <nichia@gmail.com>
2019-02-19 14:04:36 -0500

    Tidy seeds.rb and keep storage files on github

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,130 +11,155 @@ Faker::Food.unique.clear
 Faker::Internet.unique.clear
 
 # Create Categories
-Category.create(category_type: 'meal', name: 'appetizer')
-Category.create(category_type: 'meal', name: 'breakfast')
-Category.create(category_type: 'meal', name: 'entree')
-Category.create(category_type: 'meal', name: 'dessert')
-Category.create(category_type: 'meal', name: 'beverage')
-Category.create(category_type: 'meal', name: 'side')
-Category.create(category_type: 'meal', name: 'snack')
-Category.create(category_type: 'meal', name: 'salad')
-Category.create(category_type: 'meal', name: 'soup')
+def create_categories
+  puts "create_categories..." 
+  Category.create(category_type: 'meal', name: 'breakfast')
+  Category.create(category_type: 'meal', name: 'entree')
+  Category.create(category_type: 'meal', name: 'dessert')
+  Category.create(category_type: 'meal', name: 'beverage')
+  Category.create(category_type: 'meal', name: 'side')
+  Category.create(category_type: 'meal', name: 'snack')
+  Category.create(category_type: 'meal', name: 'salad')
+  Category.create(category_type: 'meal', name: 'soup')
+  Category.create(category_type: 'meal', name: 'appetizer')
+end
 
 # Create users
-avatars = Dir.glob('storage/avatars/*.*')
-icon = avatars.sample
-
-usr = User.create(
-  name: "lorem",
-  email: "lorem@email.com",
-  password: "Password1!",
-  first_name: "Lorem",
-  last_name: "Ipsum"
-) do |user|
-  Rails.root.join(icon).open('rb') do |io|
-    user.avatar.attach(io: io, filename: icon.split(/\//).last, content_type: ['image/jpg'])
- end
-end
-# do |user| user.avatar.attach(io: "File.open(icon)", filename: "icon.split(/\//).last", content_type: ['image/jpg']) end
-
-# icon = avatars.sample
-# usr.avatar.attach(io: File.open(icon), filename: icon.split(/\//).last, content_type: ['image/gif', 'image/png', 'image/jpg', 'image/jpeg'])
-
-icon = avatars.sample
-usr = User.create(
-  name: "CDion",
-  email: Faker::Internet.safe_email("celine.dion"),
-  password: "#{Faker::Internet.password(8)}A1@",
-  provider: "facebook",
-  first_name: "Celine",
-  last_name: "Dion",
-  uid: Faker::Number.between(100000, 1000000)
-) do |user|
-  Rails.root.join(icon).open('rb') do |io|
-    user.avatar.attach(io: io, filename: icon.split(/\//).last, content_type: ['image/jpg'])
- end
-end
-
-10.times do
+def create_users
+  puts "create_users..."
+  avatars = Dir.glob('storage/avatars/*.*')
   icon = avatars.sample
-  usr = User.create(
-    name: Faker::Internet.username(8, %w(. _)),
-    email: Faker::Internet.unique.safe_email,
-    password: "#{Faker::Internet.password(8)}A1@",
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name
+  User.create(
+    name: "lorem",
+    email: "lorem@email.com",
+    password: "Password1!",
+    first_name: "Lorem",
+    last_name: "Ipsum"
   ) do |user|
     Rails.root.join(icon).open('rb') do |io|
       user.avatar.attach(io: io, filename: icon.split(/\//).last, content_type: ['image/jpg'])
-   end
+    end
   end
+  print ">"
+     
+  icon = avatars.sample
+  User.create(
+    name: "CDion",
+    email: Faker::Internet.safe_email("celine.dion"),
+    password: "#{Faker::Internet.password(8)}A1@",
+    provider: "facebook",
+    first_name: "Celine",
+    last_name: "Dion",
+    uid: Faker::Number.between(100000, 1000000)
+  ) do |user|
+    Rails.root.join(icon).open('rb') do |io|
+      user.avatar.attach(io: io, filename: icon.split(/\//).last, content_type: ['image/jpg'])
+    end
+  end
+  print ">"  
+
+  10.times do
+    icon = avatars.sample
+    User.create(
+      name: Faker::Internet.username(8, %w(. _)),
+      email: Faker::Internet.unique.safe_email,
+      password: "#{Faker::Internet.password(8)}A1@",
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name
+    ) do |user|
+      Rails.root.join(icon).open('rb') do |io|
+        user.avatar.attach(io: io, filename: icon.split(/\//).last, content_type: ['image/jpg'])
+      end
+    end
+    print ">"
+  end
+  puts "."
 end
 
-# Measurement
-Measurement.create(unit: 'cup')
-Measurement.create(unit: 'teaspoon')
-Measurement.create(unit: 'tablespoon')
-Measurement.create(unit: 'bunch')
-Measurement.create(unit: 'cake')
-Measurement.create(unit: 'dash')
-Measurement.create(unit: 'drop')
-Measurement.create(unit: 'gallon')
-Measurement.create(unit: 'gram')
-Measurement.create(unit: 'handful')
-Measurement.create(unit: 'litre')
-Measurement.create(unit: 'ounce')
-Measurement.create(unit: 'packet')
-Measurement.create(unit: 'piece')
-Measurement.create(unit: 'pinch')
-Measurement.create(unit: 'pint')
-Measurement.create(unit: 'pond')
-Measurement.create(unit: 'quart')
-Measurement.create(unit: 'shot')
-Measurement.create(unit: 'splash')
-Measurement.create(unit: 'sprig')
+# Create Measurement
+def create_measurement
+  puts "create_measurement..."
+  Measurement.create(unit: 'cup')
+  Measurement.create(unit: 'teaspoon')
+  Measurement.create(unit: 'tablespoon')
+  Measurement.create(unit: 'bunch')
+  Measurement.create(unit: 'cake')
+  Measurement.create(unit: 'dash')
+  Measurement.create(unit: 'drop')
+  Measurement.create(unit: 'gallon')
+  Measurement.create(unit: 'gram')
+  Measurement.create(unit: 'handful')
+  Measurement.create(unit: 'litre')
+  Measurement.create(unit: 'ounce')
+  Measurement.create(unit: 'packet')
+  Measurement.create(unit: 'piece')
+  Measurement.create(unit: 'pinch')
+  Measurement.create(unit: 'pint')
+  Measurement.create(unit: 'pond')
+  Measurement.create(unit: 'quart')
+  Measurement.create(unit: 'shot')
+  Measurement.create(unit: 'splash')
+  Measurement.create(unit: 'sprig')
+end
 
 # Create Ingredients
-200.times do
-  Ingredient.create(name: Faker::Food.unique.ingredient.downcase)
+def create_ingredients
+  puts "create_ingredients..."
+  200.times do
+    Ingredient.create(name: Faker::Food.unique.ingredient.downcase)
+    print ">"
+  end
+  puts "."
 end
 
 # Create Recipes
-yields = ['makes', 'serves']
-preps = ['cut', 'slice', 'shred', 'squeeze', 'chopped', 'grate', 'minced']
-images = Dir.glob('storage/images/*.*')
-100.times do
-  icon = images.sample
-  recipe = Recipe.create(
-    name: "#{Faker::Food.dish.downcase} #{Faker::Lorem.unique.word}",
-    description: Faker::Food.description,
-    prep_time: Faker::Time.between(DateTime.now - 2, DateTime.now),
-    cook_time: Faker::Time.between(DateTime.now - 2, DateTime.now),
-    yields: yields.sample,
-    yields_size: Faker::Coffee.blend_name,
-    user: User.find_by_id(Faker::Number.between(1, 12))
-  ) do |r|
-    Rails.root.join(icon).open('rb') do |io|
-      r.image.attach(io: io, filename: icon.split(/\//).last, content_type: ['image/gif', 'image/png', 'image/jpg', 'image/jpeg'])
-   end
-  end
-  # recipe.image.attach(io: File.open(icon), filename: icon.split(/\//).last, content_type: ['image/gif', 'image/png', 'image/jpg', 'image/jpeg'])
+def create_recipes
+  puts "create_recipes..."
+  yields = ['makes', 'serves']
+  preps = ['cut', 'slice', 'shred', 'squeeze', 'chopped', 'grate', 'minced']
+  images = Dir.glob('storage/images/*.*')
 
-  recipe.recipe_categories.create(
-    category: Category.find_by_id(Faker::Number.between(1, 9))
-  )
+  100.times do
+    icon = images.sample
+    recipe = Recipe.create(
+      name: "#{Faker::Food.dish.downcase} #{Faker::Lorem.unique.word}",
+      description: Faker::Food.description,
+      prep_time: Faker::Time.between(DateTime.now - 2, DateTime.now),
+      cook_time: Faker::Time.between(DateTime.now - 2, DateTime.now),
+      yields: yields.sample,
+      yields_size: Faker::Coffee.blend_name,
+      user: User.find_by_id(Faker::Number.between(1, 12))
+    ) do |r|
+      Rails.root.join(icon).open('rb') do |io|
+        r.image.attach(io: io, filename: icon.split(/\//).last, content_type: ['image/gif', 'image/png', 'image/jpg', 'image/jpeg'])
+      end
+    end
 
-  5.times do
-    item = Ingredient.find_by_id(Faker::Number.between(1, 200))
-    recipe.recipe_ingredients.create(
-      quantity: Faker::Food.measurement,
-      measurement: Measurement.find_by_id(Faker::Number.between(1, 22)),
-      ingredient: item,
-      description: "#{preps.sample} #{item.name}"
+    recipe.recipe_categories.create(
+      category: Category.find_by_id(Faker::Number.between(1, 9))
     )
-  end
+  
+    5.times do
+      item = Ingredient.find_by_id(Faker::Number.between(1, 200))
+      recipe.recipe_ingredients.create(
+        quantity: Faker::Food.measurement,
+        measurement: Measurement.find_by_id(Faker::Number.between(1, 22)),
+        ingredient: item,
+        description: "#{preps.sample} #{item.name}"
+      )
+    end
+    
+    6.times do
+      recipe.instructions.create(description: Faker::Lorem.paragraph)
+    end
 
-  6.times do
-    recipe.instructions.create(description: Faker::Lorem.paragraph)
+    print ">"
   end
+  puts "."
 end
+
+create_categories
+create_ingredients
+create_measurement
+create_users
+create_recipes
