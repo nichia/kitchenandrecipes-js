54cfe4cc8b33e600d8a098cc779e49c2a21ea8b8
nichia <nichia@gmail.com>
2019-02-19 14:42:47 -0500

    Console log document.ready vs turbolinks:load

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,8 @@
+$(document).on('turbolinks:load', function () {
+  console.log('index.js Turbolinks loaded..')
+})
+
+// $(document).ready(function() {
+$(function () {
+  console.log('index.js jQuery loaded..');
+})
