e68f7bb57ecb57b3205dd1a97c987fc85987cdc4
nichia <nichia@gmail.com>
2019-02-23 20:22:41 -0500

    Add event listener on the all-recipes request

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -1,8 +1,31 @@
 $(document).on('turbolinks:load', function () {
   console.log('index.js Turbolinks loaded..')
+  listenForClickAllRecipes();
 })
 
 // $(document).ready(function() {
 $(function () {
   console.log('index.js jQuery loaded..');
 })
+
+
+function listenForClickAllRecipes() {
+  let doc = document.getElementById('all-recipes');
+
+  doc.addEventListener('click', function (event) {
+    event.preventDefault();
+    getAllRecipes();
+  });
+}
+
+function getAllRecipes() {
+  console.log('--> getAllRecipes')
+  $.ajax({
+    type: "get",
+    url: "/recipes",
+    method: "get",
+    dataType: 'json'
+  }).done(function (response) {
+    console.log('response: ', response);
+  });
+}
