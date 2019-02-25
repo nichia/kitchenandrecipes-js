6e4c43e21991b946366d23ac028e5a1c4eac16b3
nichia <nichia@gmail.com>
2019-02-24 22:09:05 -0500

    Add ListenForClickMyRecips function

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -1,17 +1,19 @@
 $(document).on('turbolinks:load', function () {
   console.log('index.js Turbolinks loaded..')
   listenForClickAllRecipes();
+  listenForClickMyRecipes();
 })
 
 // $(document).ready(function() {
-  $(function () {
+$(function () {
     console.log('index.js jQuery loaded..');
 })
 
-
 function listenForClickAllRecipes() {
+  event.preventDefault();
   // let doc = document.getElementById('all-recipes');
   // doc.addEventListener('click', function (event) {
+  // Listen for click on link with class all_recipes
   $("a.all_recipes").on("click", function (event) {
     // Fire ajax
     $.ajax({
@@ -20,14 +22,36 @@ function listenForClickAllRecipes() {
       dataType: 'json'
     }).done(function (response) {
       console.log('response: ', response);
-      debugger;
       // Get a response
-      let recipes = response;
-      let recipeObj = "";
+      getRecipes(response);
+    });
+  });
+}
 
-      // <p class="text-center card-text"><small class="text-muted"><%= pluralize(recipes.total_count, 'Recipe') %></small></p>
-
-      let htmlResp = `
+function listenForClickMyRecipes() {
+  event.preventDefault();
+  // Listen for click on link with class my_recipes
+  $("a.my_recipes").on("click", function (event) {
+    // Fire ajax
+    $.ajax({
+      method: "GET",
+      url: this.href,
+      dataType: 'json'
+    }).done(function (response) {
+      console.log('response: ', response);
+      // Get a response
+      getRecipes(response);
+    });
+  });
+}
+    
+function getRecipes(data) {
+  let recipes = data;
+  let recipeObj = "";
+  
+  // <p class="text-center card-text"><small class="text-muted"><%= pluralize(recipes.total_count, 'Recipe') %></small></p>
+  
+  let htmlResp = `
       <div class="container-fluids">
       <p class="text-center card-text"><small class="text-muted">101 Recipes</small></p>
       <div class="card-columns">`;  // Open div tags
@@ -39,35 +63,33 @@ function listenForClickAllRecipes() {
         htmlResp += getAllRecipesHtml(recipe);
       });
       htmlResp += `</div></div>`; // Close div tags
-
+      
       // Load the response into the DOM (add it to the current page)
       $("#ajax-container").html(htmlResp);
-    });
-    event.preventDefault();
-  });
-}
-
-function getAllRecipesHtml(recipe) {
-  return (`
-    <div class="card" >
-      <a href="/recipes/${recipe["id"]}"><img class="card-img-top" src="${recipe["image"]}"></a>
-      <div class="card-body">
-          <p class="card-text"><small class="text-muted">by <a href="/users/${recipe["user_id"]}">${recipe.user["name"]}</a></small></p>
-          <h4 class="card-title"><a href="/recipes/${recipe["id"]}">${recipe["name"]}</a></h4>
-          <p class="card-text">${recipe["description"]}</p>
+      
+    }
+    
+    function getAllRecipesHtml(recipe) {
+      return (`
+        <div class="card" >
+          <a href="/recipes/${recipe["id"]}"><img class="card-img-top" src="${recipe["image"]}"></a>
+          <div class="card-body">
+              <p class="card-text"><small class="text-muted">by <a href="/users/${recipe["user_id"]}">${recipe.user["name"]}</a></small></p>
+              <h4 class="card-title"><a href="/recipes/${recipe["id"]}">${recipe["name"]}</a></h4>
+              <p class="card-text">${recipe["description"]}</p>
+            </div>
         </div>
-    </div>
-  `);
-}
-
-class Recipe {
-  constructor(obj) {
-    // debugger
-    this.id = obj.id;
-    this.name = obj.name;
-    this.description = obj.description;
-    this.prep_time = obj.prep_time;
-    this.cook_time = obj.cook_time;
+      `);
+    }
+    
+    class Recipe {
+      constructor(obj) {
+        // debugger
+        this.id = obj.id;
+        this.name = obj.name;
+        this.description = obj.description;
+        this.prep_time = obj.prep_time;
+        this.cook_time = obj.cook_time;
     this.yields = obj.yields;
     this.yields_size = obj.yields_size;
     this.image = obj.image;
