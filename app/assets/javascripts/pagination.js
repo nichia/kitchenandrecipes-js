8cbeea7b1b8be316af90335f7e80219ab16cdf68
nichia <nichia@gmail.com>
2019-03-21 11:29:08 -0400

    Sort pagination links in order of first, prev, next, last

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -14,12 +14,20 @@ class Pagination {
     this.total = parseInt("headerData.total");
     if (headerData.links !== null && headerData.links !== '') {
       // this.links = headerData.links.split(',').map(item => item.trim());
-      this.links = headerData.links.split(',').reduce((accum, item) => {
+      // this.links = headerData.links.split(',').reduce((accum, item) => {
+      let link_obj = headerData.links.split(',').reduce((accum, item) => {
         // remove quotes then split by 'rel=' to get the keys (first, last, next, prev)
         const kv = item.split('"').join('').split('rel=');
         // kv[0] trim of leading/trailing spaces, remove < and >;
         return { ...accum, ...{ [kv[1]]: kv[0].trim().replace(/[<>;]/g, '') } };
       }, {})
+      const order = ["first", "prev", "next", "last"];
+      let link_array = Object.entries(link_obj); // convert object to an array
+      // sort links according to order array
+      this.links = link_array.sort(function(a,b) {
+        return order.indexOf( a[0] ) - order.indexOf( b[0] );
+      });
+      console.log("Pagelinks: ", this.links);
     };
   };
 }
